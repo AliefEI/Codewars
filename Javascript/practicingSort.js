6bd2241ecf8f25f600b235bd2898ed3f6961ee00
AliefEI <77537630+AliefEI@users.noreply.github.com>
2021-07-07 11:13:38 -0400

    update

--- Javascript/practicingSort.js
+++ Javascript/practicingSort.js
@@ -1,7 +1,11 @@
 /*
+
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
 parameter -- list of numbers
 return -- lowest number in the list
 example -- [2,6,1,9,-5]
+
 pseudocode --
   sort args by creating function with param of a,b
   return a-b --> ascending value 
