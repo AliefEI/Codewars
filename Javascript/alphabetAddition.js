c5acd80e03401257e3e379033c2f99383705ba7b
AliefEI <77537630+AliefEI@users.noreply.github.com>
2021-09-07 17:08:56 -0400

    update

--- Javascript/alphabetAddition.js
+++ Javascript/alphabetAddition.js
@@ -11,7 +11,7 @@ addLetters('a', 'b') = 'c'
 addLetters('z') = 'z'
 addLetters('z', 'a') = 'a'
 addLetters('y', 'c', 'b') = 'd' // notice the letters overflowing
-addLetters() = 'z'
+addLetters() = 'z'*/
 
 
 
