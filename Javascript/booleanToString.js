610e84d2baf23d7ce87b82c8e0258b8b6a7d085e
AliefEI <77537630+AliefEI@users.noreply.github.com>
2021-10-13 15:04:04 -0400

    update

---
+++ Javascript/booleanToString.js
@@ -0,0 +1,11 @@
+/*
+Implement a function which convert the given boolean value into its string representation.
+
+ */
+
+
+function booleanToString(b){
+    if (typeof b == 'boolean') {
+      return String(b);
+    }
+  }
