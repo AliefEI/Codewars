27f00ce3a1e61b94f25661d216fce4208b5575aa
AliefEI <77537630+AliefEI@users.noreply.github.com>
2022-03-08 17:06:26 -0500

    biweekly2

---
+++ Javascript/biweekly2.js
@@ -0,0 +1,17 @@
+/*
+parameter -- word, string
+returning -- word parameter capitalized 
+example -- object // Object 
+pseudocode --
+  - take first char 
+  - turn toUppercase
+  - add word[1] to skip fist letter 
+  - turn result into a string
+  
+*/
+
+function capitalizeWord(word) {
+    let result = word.charAt(0).toUpperCase() + word.slice(1)
+  //   console.log(result)
+    return String(result)
+  }
