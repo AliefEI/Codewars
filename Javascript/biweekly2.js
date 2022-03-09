eff99c98838be5473bf5e3dd699bc51b35e91082
AliefEI <77537630+AliefEI@users.noreply.github.com>
2022-03-09 17:20:05 -0500

    update

--- Javascript/biweekly2.js
+++ Javascript/biweekly2.js
@@ -16,4 +16,18 @@ function capitalizeWord(word) {
     return String(result)
   }
 
-  
+/*
+parameter -- s, string
+return -- string without numbers
+example -- 'frgw4t6' // frgwt
+pseudocode --
+  - create var replacing all num with empty element 
+  - turn element into a string
+*/
+
+
+function stringClean(s){
+    // Function will return the cleaned string
+    let noNumbers = s.replace(/[0-9]/g, '')
+    return String(noNumbers)
+  }
