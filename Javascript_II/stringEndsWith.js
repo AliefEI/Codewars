aa62238a880e067bb8b9cb7f13988b8a84aa1824
AliefEI <77537630+AliefEI@users.noreply.github.com>
2022-04-06 10:43:55 -0400

    endsWith Method

---
+++ Javascript_II/stringEndsWith.js
@@ -0,0 +1,27 @@
+/*
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+
+
+parameter -- 2 strings, str and ending
+returning - true or false statement
+example -- 'anti' 'ie' // false
+pseudocode --
+  endsWith method
+  
+*/
+
+function solution(str, ending){
+    // TODO: complete
+   return str.endsWith(ending)
+    
+  }
+  
+  /*
+  endsWith -- methods to help determine whether string inside of parenthesis ends with that string. if so it is true, if not false 
+  
+  */
