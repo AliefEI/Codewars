42b1002342fbbefb58bed62ab229a5535cdc889a
AliefEI <77537630+AliefEI@users.noreply.github.com>
2022-04-27 12:24:38 -0400

    prep

--- Javascript_II/sortNumbe.js
+++ Javascript_II/sortNumbe.js
@@ -1,3 +1,15 @@
+/*
+parameter - number 
+return -- ascending of num, if nums===null value // empty array
+examples 
+  [1,6,3,-4,8] // [-4,1,3,6,8]
+pseudocode 
+  determine if nums === numbers || others --> typeof
+  if other return empty array 
+  else sort value
+*/
+
+
 /*  One liner Answer    */
 
 function solution(nums){
