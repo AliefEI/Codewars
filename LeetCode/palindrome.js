90ddf8a0571c94f98b182400bbada072e00b0b15
AliefEI <77537630+AliefEI@users.noreply.github.com>
2022-04-29 12:30:40 -0400

    palindrome leertcode

---
+++ LeetCode/palindrome.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+    example -- 1221 // true 
+    pseudocode --
+    - split x but character
+    - reverse x 
+    - join x
+    - compare x to new x
+ */
+
+    var isPalindrome = function(x) {
+        //console.log(x)
+        var newX = x.toString().split('').reverse().join('')
+        return x == newX
+    }
